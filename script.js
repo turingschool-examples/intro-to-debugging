a45c707ab4c4d17ef985f50f5777c2997ab3b2d1
David Whitaker <damwhit@gmail.com>
2018-05-14 16:02:13 -0600

    Introduce syntax bug

--- script.js
+++ script.js
@@ -3,13 +3,11 @@ var input2 = document.querySelector('.js-input-2');
 var submitButton = document.querySelector('.js-submit');
 var resultArticle = document.querySelector('.js-result');
 
-submitButton.addEventListener('click', displayResult);
-
-function displayResult(event) {
+submitButton.addEventListener('click', function (event) {
   event.preventDefault();
   var num1 = parseInt(input1.value);
   var num2 = parseInt(input2.value);
   var result = num1 + num2;
   resultArticle.innerText = result;
-}
+};
 
