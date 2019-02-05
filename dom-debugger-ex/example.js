5dc7bdcc338932eadbcde6fe804262bbb66c9d9f
Khalid Williams <khalidwill27@gmail.com>
2019-02-05 14:02:47 -0700

    Add debugger dom example

---
+++ dom-debugger-ex/example.js
@@ -0,0 +1,21 @@
+var count = 0;
+var counter = document.querySelector(".counter");
+var incrementBtn = document.querySelector(".btn");
+var surpriseArticle = document.querySelector("#surprise-zone");
+
+incrementBtn.addEventListener("click", countUp);
+
+function countUp() {
+    count ++;
+    counter.innerText = count;
+    if (count % 2 === 0) {
+        alert("Get ready for a surprise");
+        surprise();
+    }
+}
+
+function surprise() { 
+    var newSuprise = document.createElement("h3")
+    newSuprise.innerText = "SURPRISE"
+    surpriseArticle.appendChild(newSuprise);
+}
